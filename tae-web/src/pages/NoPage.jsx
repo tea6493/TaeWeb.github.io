12071dfb1b22576f2e984afb9573d9982b31846d
tea6493 <Taesuk.Song@outlook.com>
2024-12-14 22:19:31 -0500

    Update NoPage.jsx

--- tae-web/src/pages/NoPage.jsx
+++ tae-web/src/pages/NoPage.jsx
@@ -3,15 +3,11 @@
 /* STYLE IMPORTS */
 
 /* COMPONENT IMPORTS */
-import Header from "../components/header/Header.jsx"
-import Footer from "../components/footer/Footer.jsx"
 
 function NoPage() {
     return (
         <>
-        <Header />
             <h1>NoPage</h1>
-        <Footer />
         </>
     )
 }
