b6fdab8d2d834dacf83b2711cb555fffe168c2bc
tea6493 <Taesuk.Song@outlook.com>
2024-12-15 02:03:57 -0500

    its time to stop
    i cant figure out how to get the back ground going.

--- tae-web/src/components/footer/Footer.jsx
+++ tae-web/src/components/footer/Footer.jsx
@@ -1,16 +1,33 @@
 
 /* React Imports */
+import { Link } from 'react-router-dom';
 
 /* Style Imports */
-import './Footer.module.css';
+import styles from './Footer.module.css';
 
 /* Component Imports */
 
+/* Asset Imports */
+import Github from '../../assets/Github.png';
+import LinkedIn from '../../assets/Linkedin.png';
+
 function Footer() {
     return (
-        <>
-            <h1>Footer</h1>
-        </>
+
+        <div className={styles.container}>
+            <h1>Wanna talk to me?</h1>
+            <h3>You can reach me at:</h3>
+            <div className={styles.link_container}>
+                <div>
+                    <Link to='https://www.linkedin.com/in/taesuk-song/'><img src={LinkedIn} alt='LinkedIn Link' className={styles.logo_icon}/></Link>
+                    <Link to='https://github.com/tea6493'><img src={Github} alt='Github Link' className={styles.logo_icon}/></Link>
+                    <h2>or</h2>
+                    <Link to='mailto:Taesuk.Song@outlook.com'><h3>Taesuk.Song@outlook.com</h3></Link>
+                </div>
+            </div>
+            <p className={styles.copyright}>&copy; {new Date().getFullYear()} Tae Web.  All Rights Reserved.</p>
+        </div>
+
     )
 }
 
--- tae-web/src/components/header/Header.jsx
+++ tae-web/src/components/header/Header.jsx
@@ -1,16 +1,59 @@
 
 /* React Imports */
+import { useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 
 /* Style Imports */
-import './Header.module.css';
+import styles from './Header.module.css';
 
 /* Component Imports */
 
+/* Asset Imports */
+import logo from '../../assets/Logo.png';
+
+const links = [
+    {
+        title: "about",
+        path: "/about",
+    },
+    {
+        title: "projects",
+        path: "/projects",
+    },
+    {
+        title: "blogs",
+        path: "/blogs",
+    },
+    {
+        title: "secrets",
+        path: "/secrets",
+    },
+];
+
 function Header() {
+
+    const [open, setOpen] = useState(false);
+
+    const pathname = useLocation().pathname;
+
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <div className={styles.container}>
+            <div>
+                <Link to='/' className={styles.logo}>
+                    <img src={logo} alt="logo" className={styles.logo_icon}/>
+                    Tae WEB
+                </Link>
+            </div>
+            <div>
+                <ul className={styles.links}>
+                    {links.map((link) =>
+                        <li>
+                            <Link to={link.path} className={`${styles.linksContainer} ${pathname === link.path && styles.active}`}>{link.title}</Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
     )
 }
 
