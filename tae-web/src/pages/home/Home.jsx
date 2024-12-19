134233503edd11072446aaf1da8aa80e3c6d64a6
tea6493 <Taesuk.Song@outlook.com>
2024-12-19 15:35:13 -0500

    home done
    finished hero sectinon and button

--- tae-web/src/pages/home/Home.jsx
+++ tae-web/src/pages/home/Home.jsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
 
 /* Component Imports */
-import Button from '../../components/button/Button';
 
 /* Asset Imports */
 
@@ -18,7 +17,7 @@ function Home() {
                 <h2>Full Stack Developer</h2>
             </div>
             <h4>Just an ordinary guy, doing an ordinary things,<br />looking for friends along the way.</h4>
-            <Link to='/about'><Button text='Learn More' color='red' /></Link>
+            <button className={styles.button}>wanna learn more about me?</button>
         </div>
     )
 }
